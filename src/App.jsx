5eec7112360f133bac8bdfa748c399baa67fb4e9
Patricio Zepeda <patriciozepeda@MacBook-Pro.local>
2024-12-01 15:02:17 -0300

    agrego archivos

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
+import { Pizza } from './pages/Pizza';
+import Cart from './pages/Cart';
+import Register_Page from './pages/Register_Page';
+import Login_Page from './pages/Login_Page';
+import { NotFound } from './components/NotFound';
+import { Profile } from './components/Profile';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pizza/:id" element={<Pizza />} />
+        <Route path="/register" element={<Register_Page />} />
+        <Route path="/login" element={<Login_Page />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
